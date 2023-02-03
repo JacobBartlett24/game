cdc1c3d2d887333219cda6793ece9fd664b1eded
JacobBartlett24 <jacobart@ttu.edu>
2023-02-02 19:36:41 -0600

    Add styling to game box

--- app/routes/game/index.tsx
+++ app/routes/game/index.tsx
@@ -1,15 +1,15 @@
-import { Box, Card } from "@chakra-ui/react";
+import { Box, Card, Grid } from "@chakra-ui/react";
 import { Outlet } from "@remix-run/react";
 import GameContainerRoute from "./GameContainer";
 import UserList from "./UserList";
 
 export default function IndexGameRoute() {
   return (
-    <>
-      <Box display={"flex"} w={"100%"} h={"100vh"} bgColor={"brand.900"} flexDirection={"row"} justifyContent={"center"} alignItems={"center"} padding={"5rem"}>
+    <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+      <Grid w={"100%"} h={"100vh"} gridTemplateRows={"100%"} gridTemplateColumns={"15rem 70rem"} justifyContent={"center"} columnGap={"10rem"} alignContent={"center"} bgColor={"brand.900"} padding={"5rem 0 5rem 0"}>
         <UserList />
         <GameContainerRoute />
-      </Box>
-    </>
+      </Grid>
+    </Box>
   );
 }
